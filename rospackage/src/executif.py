55c39cd3c8038c9d4e4dfe6039b48df851ed533a
lefake <lefake@tutanota.com>
2021-01-22 21:54:31 -0500

    DUS-361 Added joy in python and launch (not tested cause no joystick on me) and added launch args

--- rospackage/src/executif.py
+++ rospackage/src/executif.py
@@ -4,13 +4,21 @@ import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray
 from deneigus.srv import trajgen
+from enum import Enum
 
 logging = False
+
+class control_modes(Enum):
+    stop = 0
+    manual = 1
+    auto = 2
 
 class Executif:
     def __init__(self):
         # Out
+        self.pos_msg = Twist()
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.pos_tourelle_msg = Twist()
         self.cmd_tourelle_pub = rospy.Publisher('/cmd_tourelle', Twist, queue_size=10)
 
         # In
@@ -25,6 +33,23 @@ class Executif:
 
         # Services
         self.traj_serv = rospy.ServiceProxy('/trajgen_srv', trajgen)
+
+
+
+        # Variables
+        self.ctl_mode = control_modes.stop
+
+    def joy_echo(self, msg):
+        # Random axies values
+        # TODO : connect the actual joysticks values
+        throttle = msg.axes(0)
+        angle = msg.axes(1)
+
+        if self.ctl_mode == control_modes.manuel:
+            self.pos_msg.linear.x = throttle
+            self.pos_msg.angular.z = angle
+
+            self.cmd_vel_pub.publish(self.pos_msg)
 
     def pos_callback(self, msg):
         if logging:
