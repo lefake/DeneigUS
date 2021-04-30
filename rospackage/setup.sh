99d87d58675df93bdb97b8f08153b0dd584e4a08
Marc <lefake@tutanota.com>
2021-04-30 14:39:18 -0400

    Eddited the setup.sh

--- rospackage/setup.sh
+++ rospackage/setup.sh
@@ -1,1 +1,1 @@
-chmod +x src/astar.py src/executif.py src/pb2ros.py src/tf_listener.py
+chmod +x src/astar.py src/executif.py src/pb2ros.py src/tf_listener.py proto/gen_proto_arduino proto/gen_proto_python
