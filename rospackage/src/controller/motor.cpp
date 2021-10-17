5568cb18157b5d7ac9ec0ae27ff4d89391e9b7e3
lefake <lefake@tutanota.com>
2021-10-17 09:57:13 -0400

    Fix master branch

--- rospackage/src/controller/motor.cpp
+++ rospackage/src/controller/motor.cpp
@@ -1,42 +1,51 @@
-#include "motor.h"
+#include "Motor.h"
 
-
-Motor::Motor(int pin_1, int pin_2, int pwm_pin)
-{
-  _forward_pin = pin_1;
-  _backward_pin = pin_2;
-  _pwm_pin = pwm_pin;
-  stop_rotation();
-
-  // Motors setup:
-  pinMode(_forward_pin,  OUTPUT);
-  pinMode(_backward_pin,  OUTPUT);
-  pinMode(_pwm_pin, OUTPUT);
-}
+Motor::Motor() {}
+Motor::~Motor() {}
     
-void Motor::rotate(float rotation_speed)
-{ 
-   
-  int _pwm = map(abs(rotation_speed), 0, 1, 0 , 255);
-      
-  if (rotation_speed < 0){
-   // rotates backward
-   digitalWrite(_forward_pin, LOW);
-   digitalWrite(_backward_pin, HIGH);
-   analogWrite(_pwm_pin, _pwm);
-  } 
-  
-  else {
-    // rotates forward
+void Motor::init(const int forward_pin, const int backward_pin, const int speed_pin)
+{
+  _forward_pin = forward_pin;
+  _backward_pin = backward_pin;
+  _speed_pin = speed_pin;
+
+  pinMode(_forward_pin, OUTPUT);
+  pinMode(_backward_pin, OUTPUT);
+  pinMode(_speed_pin, OUTPUT);
+}
+
+void Motor::set_speed(float percentage)
+{
+
+  if (fabs(percentage) < 0.001)
+  {
+    digitalWrite(_forward_pin, LOW);
+    digitalWrite(_backward_pin, LOW);
+    analogWrite(_speed_pin, 0);
+  }
+  else if (percentage > 0)
+  {
     digitalWrite(_forward_pin, HIGH);
     digitalWrite(_backward_pin, LOW);
-    analogWrite(_pwm_pin, _pwm);
+    
+    analogWrite(_speed_pin, convert(percentage));
+  }
+  else
+  {
+    digitalWrite(_forward_pin, LOW);
+    digitalWrite(_backward_pin, HIGH);
+    
+    analogWrite(_speed_pin, convert(percentage));
   }
 }
 
-void Motor::stop_rotation()
+int Motor::convert(float percentage)
 {
-  digitalWrite(_forward_pin, LOW);
-  digitalWrite(_backward_pin, LOW);
-  analogWrite(_pwm_pin, 0);
+  float abs_val = fabs(percentage);
+  return float_map(abs_val, 0, 1, 0, 255);
+}
+
+int Motor::float_map(float x, float in_min, float in_max, float out_min, float out_max)
+{
+ return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; 
 }
--- rospackage/src/controller/motor.h
+++ rospackage/src/controller/motor.h
@@ -1,20 +1,25 @@
 #ifndef MOTOR_H
 #define MOTOR_H
-#include <Arduino.h>
+#include "Arduino.h"
 #include "constants.h"
 
-class Motor
-{
+#define CALC(l, r) (l + ((r * TRACK_DIST) / 2))
+
+class Motor {
   public:
-    Motor(int pin_1, int pin_2, int pwm_pin);
-    void rotate(float);
-    void stop_rotation();  
+    Motor();
+    ~Motor();
+
+    void init(const int forward_pin, const int backward_pin, const int speed_pin);
+    void set_speed(float);
 
   private:
     int _forward_pin;
     int _backward_pin;
-    int _pwm_pin;
-    
+    int _speed_pin;
+
+    int float_map(float x, float in_min, float in_max, float out_min, float out_max);
+    int convert(float percentage);
 };
 
 #endif
