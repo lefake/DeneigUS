cb15af038b4ad9bd622c1b16841ffbd305fe341c
pierrot32 <polagace2@hotmail.ca>
2021-11-11 09:49:47 -0500

    Fix move_base_flex and add square path

---
+++ rospackage/src/SimpleMoveBaseSquarePath.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import rospy
+import smach
+import smach_ros
+import threading
+
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
+
+from mbf_msgs.msg import ExePathAction
+from mbf_msgs.msg import GetPathAction
+from mbf_msgs.msg import RecoveryAction
+
+
+def create_pose(x, y, z, xx, yy, zz, ww):
+    pose = PoseStamped()
+    pose.header.frame_id = "map"
+    pose.header.stamp = rospy.Time.now()
+    pose.pose.position.x = x
+    pose.pose.position.y = y
+    pose.pose.position.z = z
+    pose.pose.orientation.x = xx
+    pose.pose.orientation.y = yy
+    pose.pose.orientation.z = zz
+    pose.pose.orientation.w = ww
+    return pose
+
+
+def iterate_target_poses():
+    target_poses = [
+        create_pose(0.75, 0.75, 0, 0, 0, 0, 1),
+        create_pose(0.75, -0.75, 0, 0, 0, 0.707, -0.707),
+        create_pose(-0.75, -0.75, 0, 0, 0, 1, 0),
+        create_pose(-0.75, 0.75, 0, 0, 0, 0.707, 0.707)
+    ]
+
+    for target_pose in target_poses:
+        yield target_pose
+
+def create_path_goal(path, tolerance_from_action, dist_tolerance, angle_tolerance):
+    goal = mbf_msgs.ExePathGoal()
+    goal.path = path
+    goal.tolerance_from_action = tolerance_from_action
+    goal.dist_tolerance = dist_tolerance
+    goal.angle_tolerance = angle_tolerance
+    return goal
+
+def main():
+    rospy.init_node('mbf_state_machine')
+
+    target_poses = iterate_target_poses()
+
+    # Create SMACH state machine
+    sm = smach.StateMachine(outcomes=['succeeded', 'aborted', 'preempted'])
+
+    # Define userdata
+    sm.userdata.target_pose = None
+    sm.userdata.path = None
+    sm.userdata.error = None
+    sm.userdata.clear_costmap_flag = False
+    sm.userdata.error_status = None
+
+    with sm:
+        # path callback
+        def get_path_callback(userdata, goal):
+            try:
+                goal.target_pose = next(target_poses)
+            except StopIteration:
+                rospy.logwarn("Reached last target pose")
+                rospy.signal_shutdown("Last goal reached. Shutting down")
+
+        # Get path
+        smach.StateMachine.add(
+            'GET_PATH',
+            smach_ros.SimpleActionState(
+                '/move_base_flex/get_path',
+                GetPathAction,
+                goal_cb=get_path_callback,
+                goal_slots=['target_pose'],
+                result_slots=['path']
+            ),
+            transitions={
+                'succeeded': 'EXE_PATH',
+                'aborted': 'aborted',
+                'preempted': 'preempted'
+            }
+        )
+
+        def path_callback(userdata, goal):
+            target_pose = goal.path.poses[-1].pose
+            rospy.loginfo("Attempting to reach (%1.3f, %1.3f)", target_pose.position.x, target_pose.position.y)
+
+        # Execute path
+        smach.StateMachine.add(
+            'EXE_PATH',
+            smach_ros.SimpleActionState(
+                '/move_base_flex/exe_path',
+                ExePathAction,
+                goal_cb=path_callback,
+                goal_slots=['path']
+            ),
+            transitions={
+                'succeeded': 'GET_PATH',
+                'aborted': 'aborted',
+                'preempted': 'preempted'
+            }
+        )
+
+    # Execute SMACH plan
+    # Create a thread to execute the smach container
+    smach_thread = threading.Thread(target=sm.execute)
+    smach_thread.start()
+
+    # Wait for ctrl-c
+    rospy.spin()
+
+    # Request the container to preempt
+    sm.request_preempt()
+
+    # Block until everything is preempted
+    smach_thread.join()
+
+if __name__=="__main__":
+    main()
