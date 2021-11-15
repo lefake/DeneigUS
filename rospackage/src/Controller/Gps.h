522575ac5d818f9c5159a3058ccfa15203c5caf5
Marc <lefake@tutanota.com>
2021-11-15 10:11:52 -0500

    Params clean up

--- rospackage/src/Controller/Gps.h
+++ rospackage/src/Controller/Gps.h
@@ -7,34 +7,17 @@
 #include "floatarray.pb.h"
 #include "Constants.h"
 
-#define NBS_DATA_SENT           6
-
 class Gps
 {
   public:
     Gps();
     ~Gps();
     void init();
-    void setZero();
-    long getX();
-    long getY();
-    long getZ();
-    void updateLon();
-    void updateLat();
-    long getSIVs();
-    long getAlt();
     void getCoordinates( FloatArray* msg );
 
   private:
     SFE_UBLOX_GPS myGPS;
-    long latitudeRef = 0;
-    long longitudeRef = 0;
-    long altitudeRef = 0;
-    
-    long latitude = 0;
-    long longitude = 0;
-
-    bool setupDone;
+    bool setupDone = false;
 
 };
 
