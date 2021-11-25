111635a74057cb16a3399da955adcfdf20743ce3
pierrot32 <polagace2@hotmail.ca>
2021-11-25 11:44:56 -0500

    progressmap faster

--- rospackage/src/progressMap.py
+++ rospackage/src/progressMap.py
@@ -6,14 +6,13 @@ import imageio
 import os
 import numpy as np
 
-from numpy import sin as s
-from numpy import cos as c
+from tf.listener import TransformListener
 
-from deneigus.srv import acknowledge,set_paths
+from deneigus.srv import acknowledge
 from logging_utils import setup_logger, get_logger
-from geometry_msgs.msg import PolygonStamped, PoseStamped
-from std_msgs.msg import Float32, Int32, Bool, Float32MultiArray
-from deneigus.msg import chute_msg, mbf_msg
+from geometry_msgs.msg import PointStamped
+from std_msgs.msg import Bool, Float32MultiArray
+from deneigus.msg import chute_msg
 from nav_msgs.msg import OccupancyGrid, Odometry
 from nav_msgs.srv import GetMap
 
@@ -36,19 +35,22 @@ class ProgressMap:
 
         rospy.wait_for_service('static_map')
         static_map_func = rospy.ServiceProxy('static_map', GetMap)
-        self.static_map = static_map_func()
-
-        self.progress_map = self.static_map.map
+        self.progress_map = static_map_func().map
 
         self.map = np.reshape(self.progress_map.data, (self.progress_map.info.height, self.progress_map.info.width))
 
+        self.tl = TransformListener()
+
         rospy.Subscriber("/reset_plan", Bool, self.reset_callback)
         rospy.Subscriber("/chute", Float32MultiArray, self.chute_callback)
-        rospy.Subscriber("/move_base_flex/global_costmap/footprint", PolygonStamped, self.footprint_callback)
+        rospy.Subscriber("/odometry/filtered/global", Odometry, self.odom_callback)
 
-    def reset_callback(self):
+    def reset_callback(self, _):
         self.logger.info("Reset")
-        self.progress_map = self.static_map.map
+        rospy.wait_for_service('static_map')
+        static_map_func = rospy.ServiceProxy('static_map', GetMap)
+        self.progress_map = static_map_func().map
+        self.map = np.reshape(self.progress_map.data, (self.progress_map.info.height, self.progress_map.info.width))
 
     def chute_callback(self, msg):
         if msg.data[2]>0:
@@ -56,29 +58,66 @@ class ProgressMap:
         else:
             self.in_function = False
 
-    def footprint_callback(self, msg):
-        if self.in_function :
-            self.progress_map.header.frame_id = msg.header.frame_id
-            p = msg.polygon.points
-            vertices = [(p[0].x, p[0].y), (p[1].x, p[1].y), (p[2].x, p[2].y), (p[3].x, p[3].y)]
-            x_min = int(min([p[0].x, p[1].x, p[2].x, p[3].x])/self.progress_map.info.resolution)
-            x_max = int(max([p[0].x, p[1].x, p[2].x, p[3].x])/self.progress_map.info.resolution)
-            y_min = int(min([p[0].y, p[1].y, p[2].y, p[3].y])/self.progress_map.info.resolution)
-            y_max = int(max([p[0].y, p[1].y, p[2].y, p[3].y])/self.progress_map.info.resolution)
+    def odom_callback(self, msg):
+        if self.in_function:
+            pts_1 = PointStamped()
+            pts_1.header = msg.header
+            pts_1.header.frame_id = "base_link"
+            pts_1.point.x = 0.85
+            pts_1.point.y = 0.35
 
-            for x in range(x_min, x_max+1):
-                for y in range(y_min, y_max+1):
+            pts_2 = PointStamped()
+            pts_2.header = msg.header
+            pts_2.header.frame_id = "base_link"
+            pts_2.point.x = 0.85
+            pts_2.point.y = -0.35
+
+            pts_3 = PointStamped()
+            pts_3.header = msg.header
+            pts_3.header.frame_id = "base_link"
+            pts_3.point.x = 0.4
+            pts_3.point.y = -0.35
+
+            pts_4 = PointStamped()
+            pts_4.header = msg.header
+            pts_4.header.frame_id = "base_link"
+            pts_4.point.x = 0.4
+            pts_4.point.y = 0.35
+
+            self.tl.waitForTransform("map", "base_link", msg.header.stamp, rospy.Duration(0.25))
+
+            vertice1 = self.tl.transformPoint("map", pts_1)
+            vertice2 = self.tl.transformPoint("map", pts_2)
+            vertice3 = self.tl.transformPoint("map", pts_3)
+            vertice4 = self.tl.transformPoint("map", pts_4)
+
+            p = [vertice1, vertice2, vertice3, vertice4]
+
+            vertices = [
+                (p[0].point.x, p[0].point.y),
+                (p[1].point.x, p[1].point.y),
+                (p[2].point.x, p[2].point.y),
+                (p[3].point.x, p[3].point.y)
+            ]
+
+            x_min = int(min([p[0].point.x, p[1].point.x, p[2].point.x, p[3].point.x]) / self.progress_map.info.resolution)
+            x_max = int(max([p[0].point.x, p[1].point.x, p[2].point.x, p[3].point.x]) / self.progress_map.info.resolution)
+            y_min = int(min([p[0].point.y, p[1].point.y, p[2].point.y, p[3].point.y]) / self.progress_map.info.resolution)
+            y_max = int(max([p[0].point.y, p[1].point.y, p[2].point.y, p[3].point.y]) / self.progress_map.info.resolution)
+
+            for x in range(x_min, x_max + 1):
+                for y in range(y_min, y_max + 1):
                     in_footprint = self.test_point((x + 0.5) * self.progress_map.info.resolution,
-                                                    (y + 0.5) * self.progress_map.info.resolution,
-                                                    vertices)
+                                                   (y + 0.5) * self.progress_map.info.resolution,
+                                                   vertices)
                     if in_footprint:
-                        self.map[y,x] = 50
+                        self.map[y, x] = 50
 
         self.progress_map.data = tuple(self.map.flatten())
         self.progress_map_pub.publish(self.progress_map)
 
         np_data = np.array([self.color_converter(e) for e in self.progress_map.data])
-        reshaped = np.flipud(np.reshape(np_data, (self.progress_map.info.height, self.progress_map.info.width))[25:75,25:125])
+        reshaped = np.flipud(np.reshape(np_data, (self.progress_map.info.height, self.progress_map.info.width))[25:75, 25:125])
         imageio.imwrite(self.path, reshaped, format='png', optimize=False, quantize=4)
 
     def is_on_right_side(self, x, y, xy0, xy1):
